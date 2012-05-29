58e19a7e2240dcb60cc365d4856d322bd2a991d6
Simon Maddox <simon@simonmaddox.com>
2012-05-29 15:45:55 +0100

    Initial Commit

---
+++ SMXObject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SMXObject
+//
+//  Created by Simon Maddox on 29/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
